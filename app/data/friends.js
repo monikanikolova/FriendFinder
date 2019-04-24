50ccc5e0b46d1d28097b54dab5abddbb7d1f3ea0
Monika Nikolova <Monika@Ojs-MacBook-Air.local>
2019-04-24 18:04:51 -0400

    homework

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friendsData = [
+    {
+        "name" : "Sam",
+        "photo" : "https://previews.123rf.com/images/photodeti/photodeti1502/photodeti150200008/36168922-head-cat-in-paper-side-torn-hole-isolated-on-white-background.jpg",
+        "scores" : [
+            6,
+            5,
+            3,
+            9,
+            3,
+            1,
+            6,
+            2,
+            8,
+            3
+        ]
+    }, {
+        "name" : "Bobby",
+        "photo" : "http://dzasv7x7a867v.cloudfront.net/product_photos/22194898/grey-tabby-kitty-cat-head-shaped-vinyl-animal-photo-print-clutch-bag-dotoly_original.jpg",
+        "scores ": [
+            6,
+            5,
+            3,
+            9,
+            3,
+            1,
+            6,
+            2,
+            8,
+            3
+        ]
+    }
+]
+module.exports = friendsData;
